8874d8d2e4088e9b293bf4c0f668ac46e84b6e1c
amateur-hacker <amateurhacker2004@gmail.com>
2025-04-16 10:50:45 +0530

    update theme.sh: set the default theme

--- src/themes.sh
+++ src/themes.sh
@@ -27,6 +27,29 @@ case $SELECTED_THEME in
     ["bwhite"]="#a6adc8"   # Subtext0
   )
   ;;
+*)
+  declare -A THEME=(
+    ["background"]="#1e1e2e" # Base
+    ["foreground"]="#cdd6f4" # Text
+    ["black"]="#45475a"      # Surface1
+    ["red"]="#f38ba8"        # Red
+    ["green"]="#a6e3a1"      # Green
+    ["yellow"]="#f9e2af"     # Yellow
+    ["blue"]="#89b4fa"       # Blue
+    ["magenta"]="#cba6f7"    # Mauve
+    ["cyan"]="#89dceb"       # Sky
+    ["white"]="#bac2de"      # Subtext1
+
+    ["bblack"]="#585b70"   # Surface2
+    ["bred"]="#eba0ac"     # Maroon
+    ["bgreen"]="#94e2d5"   # Teal
+    ["byellow"]="#fab387"  # Peach
+    ["bblue"]="#74c7ec"    # Sapphire
+    ["bmagenta"]="#f5c2e7" # Pink
+    ["bcyan"]="#89dceb"    # Sky (same as cyan)
+    ["bwhite"]="#a6adc8"   # Subtext0
+  )
+  ;;
 esac
 
 # Override background with "default" if transparent theme is enabled
